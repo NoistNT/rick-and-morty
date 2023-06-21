ee221ed68251701f677ef6060bbc09d01cacc9c4
NoistNT <arielgnr23@gmail.com>
2023-06-20 23:48:35 -0300

    Update character slice and handler

--- client/src/features/character/characterHandler.js
+++ client/src/features/character/characterHandler.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 const URL = 'http://localhost:3001'
@@ -13,8 +13,36 @@ export const fetchCharacters = createAsyncThunk(
 
 export const fetchCharactersByName = createAsyncThunk(
   'character/fetchCharactersByName',
-  async () => {
+  async (name) => {
     const { data } = await axios.get(`${URL}/character/?name=${name}`)
     return data.results
   }
 )
+
+export const fetchCharacterById = createAsyncThunk(
+  'character/fetchCharacterById',
+  async (id) => {
+    const { data } = await axios.get(`${URL}/character/${id}`)
+    return data
+  }
+)
+
+export const fetchCharacterByStatus = createAsyncThunk(
+  'character/fetchCharacterByStatus',
+  async (status) => {
+    const { data } = await axios.get(`${URL}/character/?status=${status}`)
+    return data.results
+  }
+)
+
+export const fetchCharacterBySpecies = createAsyncThunk(
+  'character/fetchCharacterBySpecies',
+  async (species) => {
+    const { data } = await axios.get(`${URL}/character/?species=${species}`)
+    return data.results
+  }
+)
+
+export const cleanDetail = createAction('character/cleanDetail', () => {
+  return {}
+})
--- client/src/features/character/characterSlice.js
+++ client/src/features/character/characterSlice.js
@@ -1,18 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { fetchCharacters } from './characterHandler'
+import {
+  fetchCharacters,
+  fetchCharactersByName,
+  fetchCharacterByStatus,
+  fetchCharacterBySpecies,
+  fetchCharacterById
+} from './characterHandler'
 
 const initialState = {
   allChacters: [],
   characters: [],
   character: {},
   isLoading: false,
-  error: ''
+  error: '',
+  filter: ''
 }
 
 export const characterSlice = createSlice({
   name: 'character',
   initialState,
   extraReducers: (builder) => {
+    // Fetch characters
     builder.addCase(fetchCharacters.pending, (state) => {
       state.isLoading = true
     })
@@ -26,6 +34,67 @@ export const characterSlice = createSlice({
       state.isLoading = false
       state.allChacters = []
       state.characters = []
+      state.error = action.error.message
+    })
+
+    // Fetch characters by name
+    builder.addCase(fetchCharactersByName.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchCharactersByName.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.characters = action.payload
+      state.error = ''
+    })
+    builder.addCase(fetchCharactersByName.rejected, (state, action) => {
+      state.isLoading = false
+      state.characters = []
+      state.error = action.error.message
+    })
+
+    // Fetch characters by status
+    builder.addCase(fetchCharacterByStatus.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchCharacterByStatus.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.characters = action.payload
+      state.filter = 'status'
+      state.error = ''
+    })
+    builder.addCase(fetchCharacterByStatus.rejected, (state, action) => {
+      state.isLoading = false
+      state.characters = action.payload
+      state.error = action.error.message
+    })
+
+    // Fetch characters by Species
+    builder.addCase(fetchCharacterBySpecies.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchCharacterBySpecies.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.characters = action.payload
+      state.filter = 'species'
+      state.error = ''
+    })
+    builder.addCase(fetchCharacterBySpecies.rejected, (state, action) => {
+      state.isLoading = false
+      state.error = action.error.message
+    })
+
+    // Fetch characters by ID
+    builder.addCase(fetchCharacterById.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchCharacterById.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.character = action.payload
+      state.error = ''
+    })
+    builder.addCase(fetchCharacterById.rejected, (state, action) => {
+      state.isLoading = false
+      state.character = {}
       state.error = action.error.message
     })
   }
