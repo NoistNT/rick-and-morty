3e5e7a80fb98341fff227eb792eb84b2ec267819
NoistNT <arielgnr23@gmail.com>
2023-07-30 23:16:37 -0300

    add landing page

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -4,9 +4,12 @@ export default function Landing() {
   const navigate = useNavigate()
 
   return (
-    <div>
-      <h1>Welcome to Rick and Morty App</h1>
-      <h2>Discover information about Rick and Morty characters</h2>
+    <div className='flex flex-col items-center bg-slate-600'>
+      <h1 className='pb-16'>Welcome to Rick and Morty App</h1>
+      <h3 className='text- pb-8 max-w-md'>
+        This app is designed to help you get detailed information about all your
+        favourites Rick and Morty characters
+      </h3>
       <button onClick={() => navigate('/home')}>Get started</button>
     </div>
   )
