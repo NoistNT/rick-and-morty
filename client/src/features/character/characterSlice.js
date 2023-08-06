c58d441f1ed6d7bed52bde21bc7ec3d1e540b5ca
NoistNT <arielgnr23@gmail.com>
2023-08-05 23:51:07 -0300

    add clean detail

--- client/src/features/character/characterSlice.js
+++ client/src/features/character/characterSlice.js
@@ -1,103 +1,121 @@
+// Push commit message = Add clean character detail
+
 import { createSlice } from '@reduxjs/toolkit'
 import {
   fetchCharacters,
   fetchCharactersByName,
-  fetchCharacterByStatus,
-  fetchCharacterBySpecies,
+  fetchCharactersByGender,
+  fetchCharactersBySpecies,
+  fetchCharactersByStatus,
   fetchCharacterById
 } from './characterHandler'
 
 const initialState = {
-  allChacters: [],
   characters: [],
   character: {},
   isLoading: false,
-  error: '',
-  filter: ''
+  error: ''
 }
 
 export const characterSlice = createSlice({
   name: 'character',
   initialState,
+  reducers: {
+    // Clean character details
+    cleanCharacter: (state) => {
+      state.character = {}
+    }
+  },
   extraReducers: (builder) => {
     // Fetch characters
-    builder.addCase(fetchCharacters.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(fetchCharacters.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.allChacters = action.payload
-      state.characters = action.payload
-      state.error = ''
-    })
-    builder.addCase(fetchCharacters.rejected, (state, action) => {
-      state.isLoading = false
-      state.allChacters = []
-      state.characters = []
-      state.error = action.error.message
-    })
+    builder
+      .addCase(fetchCharacters.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchCharacters.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.characters = payload
+        state.error = ''
+      })
+      .addCase(fetchCharacters.rejected, (state, { error }) => {
+        state.isLoading = false
+        state.characters = []
+        state.error = error.message
+      })
 
-    // Fetch characters by name
-    builder.addCase(fetchCharactersByName.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(fetchCharactersByName.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.characters = action.payload
-      state.error = ''
-    })
-    builder.addCase(fetchCharactersByName.rejected, (state, action) => {
-      state.isLoading = false
-      state.characters = []
-      state.error = action.error.message
-    })
+      // Fetch characters by name
+      .addCase(fetchCharactersByName.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchCharactersByName.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.characters = payload
+        state.error = ''
+      })
+      .addCase(fetchCharactersByName.rejected, (state, { error }) => {
+        state.isLoading = false
+        state.characters = []
+        state.error = error.message
+      })
 
-    // Fetch characters by status
-    builder.addCase(fetchCharacterByStatus.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(fetchCharacterByStatus.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.characters = action.payload
-      state.filter = 'status'
-      state.error = ''
-    })
-    builder.addCase(fetchCharacterByStatus.rejected, (state, action) => {
-      state.isLoading = false
-      state.characters = action.payload
-      state.error = action.error.message
-    })
+      // Fetch characters by gender
+      .addCase(fetchCharactersByGender.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchCharactersByGender.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.characters = payload
+        state.error = ''
+      })
+      .addCase(fetchCharactersByGender.rejected, (state, { error }) => {
+        state.isLoading = false
+        state.characters = []
+        state.error = error.message
+      })
 
-    // Fetch characters by Species
-    builder.addCase(fetchCharacterBySpecies.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(fetchCharacterBySpecies.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.characters = action.payload
-      state.filter = 'species'
-      state.error = ''
-    })
-    builder.addCase(fetchCharacterBySpecies.rejected, (state, action) => {
-      state.isLoading = false
-      state.error = action.error.message
-    })
+      // Fetch characters by species
+      .addCase(fetchCharactersBySpecies.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchCharactersBySpecies.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.characters = payload
+        state.error = ''
+      })
+      .addCase(fetchCharactersBySpecies.rejected, (state, { error }) => {
+        state.isLoading = false
+        state.error = error.message
+      })
 
-    // Fetch characters by ID
-    builder.addCase(fetchCharacterById.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(fetchCharacterById.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.character = action.payload
-      state.error = ''
-    })
-    builder.addCase(fetchCharacterById.rejected, (state, action) => {
-      state.isLoading = false
-      state.character = {}
-      state.error = action.error.message
-    })
+      // Fetch characters by status
+      .addCase(fetchCharactersByStatus.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchCharactersByStatus.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.characters = payload
+        state.error = ''
+      })
+      .addCase(fetchCharactersByStatus.rejected, (state, { error }) => {
+        state.isLoading = false
+        state.error = error.message
+      })
+
+      // Fetch characters by ID
+      .addCase(fetchCharacterById.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchCharacterById.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.character = payload
+        state.error = ''
+      })
+      .addCase(fetchCharacterById.rejected, (state, { error }) => {
+        state.isLoading = false
+        state.error = error.message
+      })
   }
 })
 
+export const { cleanCharacter } = characterSlice.actions
 export default characterSlice.reducer
