e48eb4739addf9c651a776832d6efeac9b471f3f
NoistNT <arielgnr23@gmail.com>
2023-08-05 23:50:48 -0300

    update external api to own api

--- client/src/features/character/characterHandler.js
+++ client/src/features/character/characterHandler.js
@@ -1,48 +1,76 @@
-import { createAction, createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const URL = 'http://localhost:3001'
+const URL = 'https://ricknmortyback.onrender.com'
 
 export const fetchCharacters = createAsyncThunk(
   'character/fetchCharacters',
   async () => {
-    const { data } = await axios.get(`${URL}/character`)
-    return data.results
+    try {
+      const { data } = await axios.get(`${URL}/character`)
+      return data.results
+    } catch (error) {
+      throw new Error('Failed to fetch characters', error.message)
+    }
   }
 )
 
 export const fetchCharactersByName = createAsyncThunk(
   'character/fetchCharactersByName',
   async (name) => {
-    const { data } = await axios.get(`${URL}/character/?name=${name}`)
-    return data.results
+    try {
+      const { data } = await axios.get(`${URL}/character/?name=${name}`)
+      return data.results
+    } catch (error) {
+      throw new Error('Failed to fetch characters by name', error.message)
+    }
+  }
+)
+
+export const fetchCharactersByGender = createAsyncThunk(
+  'character/fetchCharactersByGender',
+  async (gender) => {
+    try {
+      const { data } = await axios.get(`${URL}/character/?gender=${gender}`)
+      return data.results
+    } catch (error) {
+      throw new Error('Failed to fetch characters by gender', error.message)
+    }
+  }
+)
+
+export const fetchCharactersBySpecies = createAsyncThunk(
+  'character/fetchCharactersBySpecies',
+  async (species) => {
+    try {
+      const { data } = await axios.get(`${URL}/character/?species=${species}`)
+      return data.results
+    } catch (error) {
+      throw new Error('Failed to fetch characters by species', error.message)
+    }
+  }
+)
+
+export const fetchCharactersByStatus = createAsyncThunk(
+  'character/fetchCharactersByStatus',
+  async (status) => {
+    try {
+      const { data } = await axios.get(`${URL}/character/?status=${status}`)
+      return data.results
+    } catch (error) {
+      throw new Error('Failed to fetch characters by status', error.message)
+    }
   }
 )
 
 export const fetchCharacterById = createAsyncThunk(
   'character/fetchCharacterById',
   async (id) => {
-    const { data } = await axios.get(`${URL}/character/${id}`)
-    return data
+    try {
+      const { data } = await axios.get(`${URL}/character/${id}`)
+      return data
+    } catch (error) {
+      throw new Error('Failed to fetch character by ID', error.message)
+    }
   }
 )
-
-export const fetchCharacterByStatus = createAsyncThunk(
-  'character/fetchCharacterByStatus',
-  async (status) => {
-    const { data } = await axios.get(`${URL}/character/?status=${status}`)
-    return data.results
-  }
-)
-
-export const fetchCharacterBySpecies = createAsyncThunk(
-  'character/fetchCharacterBySpecies',
-  async (species) => {
-    const { data } = await axios.get(`${URL}/character/?species=${species}`)
-    return data.results
-  }
-)
-
-export const cleanDetail = createAction('character/cleanDetail', () => {
-  return {}
-})
