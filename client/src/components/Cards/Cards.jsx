072a7adf1f690819f098610319277a9ff745cf6d
NoistNT <arielgnr23@gmail.com>
2023-08-20 23:55:49 -0300

    update cards styles

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
-import Card from '../Card/Card'
+import Card from "../Card/Card";
 
 export default function Cards({ characters }) {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
       {characters.isLoading && <h1>Loading...</h1>}
       {characters.error && <div>Error: {characters.error} </div>}
       {characters.map((character) => (
         <Card key={character.id} character={character} />
       ))}
     </div>
-  )
+  );
 }
