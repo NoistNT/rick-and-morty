f39c7663761130bfdfae9a61d8d3274885a3e658
NoistNT <arielgnr23@gmail.com>
2023-08-20 23:58:38 -0300

    update menu styles

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,13 +1,13 @@
-import FilterByGender from './Filters/FilterByGender'
-import FilterBySpecies from './Filters/FilterBySpecies'
-import FilterByStatus from './Filters/FilterByStatus'
+import FilterByGender from "./Filters/FilterByGender";
+import FilterBySpecies from "./Filters/FilterBySpecies";
+import FilterByStatus from "./Filters/FilterByStatus";
 
 export default function Menu() {
   return (
-    <div>
+    <div className="flex justify-center items-center gap-6">
       <FilterByGender />
       <FilterBySpecies />
       <FilterByStatus />
     </div>
-  )
+  );
 }
