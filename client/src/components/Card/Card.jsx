865ce9503822091ee71f182e10df1d126dd736b0
NoistNT <arielgnr23@gmail.com>
2023-08-20 23:55:33 -0300

    update card styles

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Card({ character }) {
   const { id, name, species, gender, image } = character;
   return (
-    <div className="flex bg-white rounded-lg my-8 p-4">
+    <div className="flex bg-white rounded-lg p-4">
       <Link to={`/character/${id}`}>
         <img src={image} alt={name} />
         <h2>{name}</h2>
