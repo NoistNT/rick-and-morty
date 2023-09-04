04fcc07ba523c59747c03c51f6d62fb8a38a232a
NoistNT <arielgnr23@gmail.com>
2023-09-04 20:16:05 -0300

    add characterApi in RTK Query

--- src/redux/api/characterApi.ts
+++ src/redux/api/characterApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-type Character = {
+interface Character {
   id: number
   name: string
   // status: string
@@ -21,17 +21,31 @@ type Character = {
   // created: string
 }
 
+interface CharactersResponse {
+  info: {
+    count: number
+    pages: number
+    next: string
+    prev: string
+  }
+  results: Character[]
+}
+
 export const characterApi = createApi({
   reducerPath: 'characterApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://rickandmortyapi.com/api/'
+  }),
   endpoints: (builder) => ({
-    getCharacters: builder.query<Character[], null>({
-      query: () => 'characters'
+    getCharacters: builder.query<CharactersResponse, number>({
+      query: (page = 1) => `character/?page=${page}`
     }),
-    getCharacterById: builder.query<Character, { id: number }>({
+    getCharacterById: builder.query<CharactersResponse, { id: number }>({
       query: ({ id }) => `character/${id}`
     })
   })
 })
 
-export const { useGetCharactersQuery, useGetCharacterByIdQuery } = characterApi
+export const { useGetCharactersQuery, useGetCharacterByIdQuery, reducerPath } =
+  characterApi
+export default characterApi
