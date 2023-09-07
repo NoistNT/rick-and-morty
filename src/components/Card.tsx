66f16ba7be37e5422d0baa41807d3e0ed2e94cba
NoistNT <arielgnr23@gmail.com>
2023-09-07 09:30:01 -0300

    update card component imports

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { Character } from '@/redux/api/characterApi'
+import { Character } from '@/types'
 
 export default function Card({ character }: { character: Character }) {
   return (
