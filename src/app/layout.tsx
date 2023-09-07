bcaf021a44b62825f606f33de2d95d70e8be0b17
NoistNT <arielgnr23@gmail.com>
2023-09-07 09:29:49 -0300

    add footer into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from '@/redux/Providers'
 import Nav from '@/components/Nav'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Nav />
         <Providers>{children}</Providers>
+        <Footer />
       </body>
     </html>
   )
