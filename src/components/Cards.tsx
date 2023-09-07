d674ca366a7f0c7833ece6135b17e4d32a763c57
NoistNT <arielgnr23@gmail.com>
2023-09-07 18:53:04 -0300

    update cards component with paginate

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,10 +1,22 @@
 'use client'
 
 import { useGetCharactersQuery } from '@/redux/api/characterApi'
+import { useState } from 'react'
 import Card from '@/components/Card'
 
 export default function Cards() {
-  const { data, error, isLoading } = useGetCharactersQuery(1)
+  const [currentPage, setCurrentPage] = useState(1)
+  const { data, error, isLoading } = useGetCharactersQuery({
+    page: currentPage
+  })
+
+  const prevPage = data?.info?.prev?.split('=').pop()
+  const nextPage = data?.info?.next?.split('=').pop()
+
+  const handlePageChange = (page: number) => {
+    console.log(page)
+    setCurrentPage(page)
+  }
 
   if (isLoading) {
     return <h2 className='text-xl'>Loading...</h2>
@@ -15,10 +27,18 @@ export default function Cards() {
   }
 
   return (
-    <section className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl'>
-      {data?.results.map((character) => (
-        <Card key={character.id} character={character} />
-      ))}
+    <section>
+      <button onClick={() => handlePageChange(Number(prevPage))}>
+        Prev Page
+      </button>
+      <button onClick={() => handlePageChange(Number(nextPage))}>
+        Next Page
+      </button>
+      <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl'>
+        {data?.results.map((character) => (
+          <Card key={character.id} character={character} />
+        ))}
+      </div>
     </section>
   )
 }
