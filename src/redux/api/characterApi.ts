0c1bc200816cd00d10822c1029b161edc363d184
NoistNT <arielgnr23@gmail.com>
2023-09-07 09:30:31 -0300

    move interfaces to types file

--- src/redux/api/characterApi.ts
+++ src/redux/api/characterApi.ts
@@ -1,43 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-export interface Character {
-  id: number
-  name: string
-  species: string
-  gender: string
-  image: string
-}
-
-interface CharacterDetail {
-  id: number
-  name: string
-  status: string
-  species: string
-  type: string
-  gender: string
-  origin: {
-    name: string
-    url: string
-  }
-  location: {
-    name: string
-    url: string
-  }
-  image: string
-  episode: string[]
-  url: string
-  created: string
-}
-
-interface CharactersResponse {
-  info: {
-    count: number
-    pages: number
-    next: string | null
-    prev: string | null
-  }
-  results: Character[]
-}
+import { CharacterDetail, CharactersResponse } from '@/types'
 
 export const characterApi = createApi({
   reducerPath: 'characterApi',
