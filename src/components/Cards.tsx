01c6f550575e4feb4f3e42adcfba753b36c4ad37
NoistNT <arielgnr23@gmail.com>
2023-09-09 01:52:18 -0300

    update cards component to work with paginate component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,23 +1,19 @@
 'use client'
 
 import { useGetCharactersQuery } from '@/redux/api/characterApi'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import Card from '@/components/Card'
-import { setCurrentPage } from '@/redux/features/character/characterSlice'
+import { useAppSelector } from '@/redux/hooks'
+import { Character, PageProps } from '@/types'
+import Card from './Card'
+import Paginate from './Paginate'
 
-export default function Cards() {
-  const dispatch = useAppDispatch()
+export default function Cards(): JSX.Element {
   const { currentPage } = useAppSelector((state) => state.character)
   const { data, error, isLoading } = useGetCharactersQuery({
     page: currentPage
   })
 
-  const prevPage = data?.info?.prev?.split('=').pop()
-  const nextPage = data?.info?.next?.split('=').pop()
-
-  const handlePageChange = (page: number) => {
-    dispatch(setCurrentPage(page))
-  }
+  const prevPage: PageProps = data?.info?.prev?.split('=').pop()
+  const nextPage: PageProps = data?.info?.next?.split('=').pop()
 
   if (isLoading) {
     return <h2 className='text-xl'>Loading...</h2>
@@ -29,17 +25,13 @@ export default function Cards() {
 
   return (
     <section>
-      <button onClick={() => handlePageChange(Number(prevPage))}>
-        Prev Page
-      </button>
-      <button onClick={() => handlePageChange(Number(nextPage))}>
-        Next Page
-      </button>
+      <Paginate prevPage={prevPage} nextPage={nextPage} />
       <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl'>
-        {data?.results.map((character) => (
+        {data?.results.map((character: Character) => (
           <Card key={character.id} character={character} />
         ))}
       </div>
+      <Paginate prevPage={prevPage} nextPage={nextPage} />
     </section>
   )
 }
