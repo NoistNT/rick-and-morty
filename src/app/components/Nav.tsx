0f2ddb02c35584c35de0bc2af5c6bf22466570bc
NoistNT <arielgnr23@gmail.com>
2023-09-11 10:02:32 -0300

    move components folder into app folder

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import SearchBar from '@/app/components/SearchBar'
+
+export default function Nav() {
+  return (
+    <nav className='flex py-6 px-3 items-center gap-4 bg-indigo-800'>
+      <Link href='/'>
+        <h1 className='text-3xl font-bold text-white'>Rick and Morty</h1>
+      </Link>
+      <ul className='flex gap-4'>
+        <li>
+          <Link href='/character'>Home</Link>
+        </li>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+        <li>
+          <Link href='/counter'>Counter</Link>
+        </li>
+      </ul>
+      <SearchBar />
+    </nav>
+  )
+}
---
+++ src/app/components/Paginate.tsx
@@ -0,0 +1,41 @@
+import { useAppDispatch } from '@/redux/hooks'
+import { setCurrentPage } from '@/redux/features/character/characterSlice'
+import { PaginationProps } from '@/types'
+
+export default function Paginate({
+  prevPage,
+  nextPage,
+  lastPage
+}: PaginationProps): JSX.Element {
+  const dispatch = useAppDispatch()
+
+  const handlePageChange = (page: number): void => {
+    dispatch(setCurrentPage(page))
+  }
+
+  return (
+    <div className='flex justify-center items-center gap-4 bg-slate-600 py-2 my-4 rounded-md'>
+      <button disabled={!prevPage} onClick={() => handlePageChange(1)}>
+        First
+      </button>
+      <button
+        disabled={!prevPage}
+        onClick={() => handlePageChange(Number(prevPage))}
+      >
+        Prev
+      </button>
+      <button
+        disabled={!nextPage}
+        onClick={() => handlePageChange(Number(nextPage))}
+      >
+        Next
+      </button>
+      <button
+        disabled={!nextPage}
+        onClick={() => handlePageChange(Number(lastPage))}
+      >
+        Last
+      </button>
+    </div>
+  )
+}
