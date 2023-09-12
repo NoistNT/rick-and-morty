e6964edd938f4f5d2c7aa0d450c3d29a3a4cf821
NoistNT <arielgnr23@gmail.com>
2023-09-12 10:13:13 -0300

    remove unnecessary div

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -2,10 +2,8 @@ import Characters from '../components/Characters'
 
 export default function Character() {
   return (
-    <div>
-      <main className='flex min-h-screen flex-col items-center my-10'>
-        <Characters />
-      </main>
-    </div>
+    <main className='flex min-h-screen flex-col items-center my-10'>
+      <Characters />
+    </main>
   )
 }
