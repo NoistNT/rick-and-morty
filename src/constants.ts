8e25c20d1b58f85f8281df9e67406e19ef5de385
NoistNT <arielgnr23@gmail.com>
2023-12-12 11:51:45 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,1 @@
-export const API_INFO = 'https://rickandmortyapi.com/api'
-
-export const CHARACTERS_LIST = 'https://rickandmortyapi.com/api/character'
+export const API_INFO = 'https://rickandmortyapi.com/api/character'
