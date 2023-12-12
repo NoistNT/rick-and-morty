608c619144fb167223461ba1cd2f18f365231f33
NoistNT <arielgnr23@gmail.com>
2023-12-12 11:52:48 -0300

    Update pagination and search, add debounce

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,8 +1,70 @@
-export default function Pagination(): JSX.Element {
+'use client'
+
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
+
+export default function Pagination({ page, pages }: { page: number; pages: number }): JSX.Element {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const params: URLSearchParams = new URLSearchParams(searchParams)
+
+  const handlePageChange = (page: number): void => {
+    params.set('page', page.toString())
+
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
-    <div className="my-10 flex items-center justify-center gap-4 rounded-md bg-slate-800 py-2 text-white md:gap-7">
-      <p>Previous</p>
-      <p>Next</p>
+    <div className="my-10 flex w-full items-center justify-center gap-4 rounded-md bg-slate-800 py-2 text-white md:gap-7">
+      <button
+        className={
+          page === 1
+            ? 'text-neutral-500 transition-colors'
+            : 'opacity-80 transition-colors hover:opacity-100'
+        }
+        disabled={page === 1}
+        type="button"
+        onClick={() => handlePageChange(1)}
+      >
+        First
+      </button>
+      <button
+        className={
+          page === 1
+            ? 'text-neutral-500 transition-colors'
+            : 'opacity-80 transition-colors hover:opacity-100'
+        }
+        disabled={page === 1}
+        type="button"
+        onClick={() => handlePageChange(Number(params.get('page')) - 1)}
+      >
+        Prev
+      </button>
+      <button
+        className={
+          page === pages
+            ? 'text-neutral-500 transition-colors'
+            : 'opacity-80 transition-colors hover:opacity-100'
+        }
+        disabled={page === pages}
+        type="button"
+        onClick={() => handlePageChange(Number(params.get('page')) + 1)}
+      >
+        Next
+      </button>
+      <button
+        className={
+          page === pages
+            ? 'text-neutral-500 transition-colors'
+            : 'opacity-80 transition-colors hover:opacity-100'
+        }
+        disabled={page === pages}
+        type="button"
+        onClick={() => handlePageChange(pages)}
+      >
+        Last
+      </button>
     </div>
   )
 }
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -2,35 +2,38 @@
 
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function SearchBar({ placeholder }: { placeholder: string }): JSX.Element {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  const handleSearch = async (term: string) => {
-    const params = new URLSearchParams(searchParams)
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params: URLSearchParams = new URLSearchParams(searchParams)
 
     if (term) {
-      params.set('query', term)
+      params.set('name', term)
+      params.set('page', '1')
     } else {
-      params.delete('query')
+      params.delete('name')
+      params.set('page', '1')
     }
 
     replace(`${pathname}?${params.toString()}`)
-  }
+  }, 300)
 
   return (
     <div className="relative flex w-full max-w-[300px] items-center gap-2 rounded-md border-2 border-white/20 bg-neutral-900 bg-opacity-70 px-2 py-1 shadow-lg transition-all duration-200 ease-in-out hover:border-white/30 sm:max-w-xs lg:max-w-lg">
       <input
         className="w-full rounded-md bg-transparent text-center text-[0.95rem] text-neutral-300 placeholder-neutral-500 outline-none transition-all duration-200 ease-in-out placeholder-shown:text-neutral-400 focus:placeholder-shown:text-neutral-700"
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get('name')?.toString()}
         placeholder={placeholder}
         type="text"
         onChange={(event) => handleSearch(event.target.value)}
       />
       <FaMagnifyingGlass
-        className="absolute right-4 top-1/2 -translate-y-1/2 transform text-neutral-500 transition-all duration-200 ease-in-out hover:text-neutral-700"
+        className="absolute right-4 top-1/2 -translate-y-1/2 transform text-neutral-700 transition-all duration-200 ease-in-out"
         size={20}
       />
     </div>
