95d4f27e5a50a40c80d1b4fccdcac3c4d51a5f0c
NoistNT <arielgnr23@gmail.com>
2023-12-18 10:05:06 -0300

    Update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="pb-6 text-center leading-[3rem] opacity-70 ">
-      © {new Date().getFullYear()} Rick And Morty Unofficial App
+      {new Date().getFullYear()} Rick And Morty Unofficial App
     </footer>
   )
 }
