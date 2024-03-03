004d3ee24166787fe02f8871d2f0aa33a8ecbf07
NoistNT <arielgnr23@gmail.com>
2024-03-02 21:12:01 -0300

    Update card component

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,17 +1,12 @@
 import type { Character } from '@/types'
 
-import { Suspense } from 'react'
-
 import Card from './Card'
-import CardSkeleton from './Skeletons/CardSkeleton'
 
 export default function Cards({ characters }: { characters: Character[] }): JSX.Element {
   return (
     <div className="grid max-w-7xl grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {characters.map((character: Character) => (
-        <Suspense key={character.id} fallback={<CardSkeleton />}>
-          <Card key={character.id} character={character} />
-        </Suspense>
+        <Card key={character.id} character={character} />
       ))}
     </div>
   )
