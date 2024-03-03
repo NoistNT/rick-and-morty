a7a505003cdcfe1906385b5cea5b9cdb0e1c6e76
NoistNT <arielgnr23@gmail.com>
2024-03-02 21:41:38 -0300

    Add Suspense to searchbar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 import SearchBar from '@/app/components/SearchBar'
 
 export default function Nav() {
   return (
     <nav className="sticky top-0 z-50 flex flex-col items-center justify-between gap-2 bg-slate-900/50 py-2 backdrop-blur-md sm:flex-row sm:px-6 md:px-8 md:pb-0 md:pt-2">
-      <Link className="transform transition-transform hover:scale-105" href="/">
+      <Link className="transform transition-transform hover:scale-105" href="/?page=1">
         <Image alt="Rick and Morty Logo" height={67} src="/rickandmortylogo.webp" width={200} />
       </Link>
-      <SearchBar placeholder="Search character" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchBar placeholder="Search character" />
+      </Suspense>
     </nav>
   )
 }
