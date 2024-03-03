8aca8e2faaae1321ac99af2cdfc3f26672857821
NoistNT <arielgnr23@gmail.com>
2024-03-02 21:12:29 -0300

    Add type annotations

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,51 +1,68 @@
-import { Character, CharacterDetail, PaginationInfo } from '@/types'
+import { ApiInfoResponse, Character, CharacterDetail, PaginationInfo } from '@/types'
 
-import { API_INFO } from '../../constants'
+const { API_INFO } = process.env
 
 export const getApiInfo = async (name?: string): Promise<PaginationInfo> => {
   const res = await fetch(`${API_INFO}?name=${name}`, { cache: 'no-store' })
-  const data = await res.json()
+  const data = (await res.json()) as ApiInfoResponse
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data', data.error)
+    throw new Error('Failed to fetch data')
   }
 
   return data.info
 }
 
-export const getCharacters = async (name?: string, page?: number | string): Promise<Character[]> => {
-  if (name) {
-    return await getCharacterByName(name, page)
-  }
+export const getCharacters = async (
+  name?: string,
+  page?: number | string
+): Promise<Character[]> => {
+  if (name) return await getCharacterByName(name, page)
 
   const res = await fetch(`${API_INFO}?page=${page}`, { cache: 'no-store' })
-  const data = await res.json()
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data', data.error)
+    throw new Error('Failed to fetch data')
   }
 
-  return data.results
+  const data = (await res.json()) as ApiInfoResponse
+  const characters = data.results.map((character: Character) => {
+    return {
+      id: character.id,
+      name: character.name,
+      species: character.species,
+      gender: character.gender,
+      image: character.image
+    }
+  })
+
+  return characters
 }
 
 export const getCharacterById = async (id: number): Promise<CharacterDetail> => {
   const res = await fetch(`${API_INFO}/${id}`, { cache: 'no-store' })
-  const data = await res.json()
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data', data.error)
+    throw new Error('Failed to fetch data')
   }
 
-  return data
+  const character = (await res.json()) as CharacterDetail
+
+  return character
 }
 
-export const getCharacterByName = async (name: string, page?: number | string): Promise<Character[]> => {
+export const getCharacterByName = async (
+  name: string,
+  page?: number | string
+): Promise<Character[]> => {
   const res = await fetch(`${API_INFO}?page=${page}&name=${name}`, { cache: 'no-store' })
-  const data = await res.json()
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data', data.error)
+    throw new Error('Failed to fetch data')
   }
 
-  return data.results
+  const data = (await res.json()) as ApiInfoResponse
+  const character = data.results
+
+  return character
 }
